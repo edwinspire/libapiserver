149920fd45d22c6415aa9832e97798a9743708e5
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz@farmaenlace.com>
2023-06-28 09:00:14 -0500

    se coloca nombre a la librería

--- build/server/chunks/1-012d2bad.js
+++
@@ -1,8 +0,0 @@
-const index = 1;
-const component = async () => (await import('./error.svelte-252844dd.js')).default;
-const imports = ["_app/immutable/nodes/1.cfa0cc98.js","_app/immutable/chunks/index.a0f43e26.js","_app/immutable/chunks/singletons.1c7d2b46.js","_app/immutable/chunks/index.349093ea.js"];
-const stylesheets = [];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=1-012d2bad.js.map
--- build/server/manifest.js
+++ build/server/manifest.js
@@ -4,10 +4,10 @@ const manifest = {
 	assets: new Set(["css/bulma/bulma-rtl.css","css/bulma/bulma-rtl.css.map","css/bulma/bulma-rtl.min.css","css/bulma/bulma.css","css/bulma/bulma.css.map","css/bulma/bulma.min.css","css/fontawesome/css/all.css","css/fontawesome/css/all.min.css","css/fontawesome/css/brands.css","css/fontawesome/css/brands.min.css","css/fontawesome/css/fontawesome.css","css/fontawesome/css/fontawesome.min.css","css/fontawesome/css/regular.css","css/fontawesome/css/regular.min.css","css/fontawesome/css/solid.css","css/fontawesome/css/solid.min.css","css/fontawesome/css/svg-with-js.css","css/fontawesome/css/svg-with-js.min.css","css/fontawesome/css/v4-font-face.css","css/fontawesome/css/v4-font-face.min.css","css/fontawesome/css/v4-shims.css","css/fontawesome/css/v4-shims.min.css","css/fontawesome/css/v5-font-face.css","css/fontawesome/css/v5-font-face.min.css","css/fontawesome/webfonts/fa-brands-400.ttf","css/fontawesome/webfonts/fa-brands-400.woff2","css/fontawesome/webfonts/fa-regular-400.ttf","css/fontawesome/webfonts/fa-regular-400.woff2","css/fontawesome/webfonts/fa-solid-900.ttf","css/fontawesome/webfonts/fa-solid-900.woff2","css/fontawesome/webfonts/fa-v4compatibility.ttf","css/fontawesome/webfonts/fa-v4compatibility.woff2","favicon.png","robots.txt","svelte-welcome.png","svelte-welcome.webp"]),
 	mimeTypes: {".css":"text/css",".map":"application/json",".ttf":"font/ttf",".woff2":"font/woff2",".png":"image/png",".txt":"text/plain",".webp":"image/webp"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.09601c38.js","app":"_app/immutable/entry/app.15f0d83b.js","imports":["_app/immutable/entry/start.09601c38.js","_app/immutable/chunks/index.a0f43e26.js","_app/immutable/chunks/singletons.1c7d2b46.js","_app/immutable/chunks/index.349093ea.js","_app/immutable/entry/app.15f0d83b.js","_app/immutable/chunks/index.a0f43e26.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.9e527b28.js","app":"_app/immutable/entry/app.c1593822.js","imports":["_app/immutable/entry/start.9e527b28.js","_app/immutable/chunks/index.a0f43e26.js","_app/immutable/chunks/singletons.3e7b040d.js","_app/immutable/chunks/index.349093ea.js","_app/immutable/entry/app.c1593822.js","_app/immutable/chunks/index.a0f43e26.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			() => import('./chunks/0-7642a59b.js'),
-			() => import('./chunks/1-012d2bad.js'),
+			() => import('./chunks/1-1cd92610.js'),
 			() => import('./chunks/3-0cca8973.js')
 		],
 		routes: [
