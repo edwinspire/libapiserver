1263d9316886694b5449be3dbb58dde37edae8b4
Edwin De La Cruz <edwinspire@gmail.com>
2023-08-23 00:50:03 -0500

    Correccion de bug en JS

--- src/lib/api/gui/app/vars.svelte
+++ src/lib/api/gui/app/vars.svelte
@@ -1,35 +1,35 @@
 <script>
-  // @ts-nocheck
-  import { onMount } from "svelte";
-  import { listAppVars } from "../utils";
-  import EditorCode from "./handler/editorCode.svelte";
-  import CodeHTML from "../widgets/codeHTML.svelte";
+	// @ts-nocheck
+	import { onMount } from 'svelte';
+	import { listAppVars } from '../utils';
+	import EditorCode from './handler/editorCode.svelte';
+	import CodeHTML from '../widgets/codeHTML.svelte';
 
-  // export let vars = {};
-  export let editable = false;
-  let Datavars = {};
+	// export let vars = {};
+	export let editable = false;
+	let Datavars = {};
 
-  listAppVars.subscribe((value) => {
-    console.log("listAppVars ->>>>", value, JSON.stringify(value, null, 4));
-    // @ts-ignore
-    Datavars = value;
-  });
+	listAppVars.subscribe((value) => {
+		console.log('listAppVars ->>>>', value, JSON.stringify(value, null, 4));
+		// @ts-ignore
+		Datavars = value || {};
+	});
 
-  onMount(() => {});
+	onMount(() => {});
 </script>
 
 <div>
-  {#each Object.keys(Datavars) as varKey}
-    {#if editable}
-      <div>{varKey}</div>
-      <div>
-        <EditorCode lang={"json"} code={JSON.stringify(Datavars[varKey])} />
-      </div>
-    {:else}
-      <details>
-        <summary><strong>{varKey}</strong></summary>
-        <CodeHTML bind:jsonObject={Datavars[varKey]} />
-      </details>
-    {/if}
-  {/each}
+	{#each Object.keys(Datavars) as varKey}
+		{#if editable}
+			<div>{varKey}</div>
+			<div>
+				<EditorCode lang={'json'} code={JSON.stringify(Datavars[varKey])} />
+			</div>
+		{:else}
+			<details>
+				<summary><strong>{varKey}</strong></summary>
+				<CodeHTML bind:jsonObject={Datavars[varKey]} />
+			</details>
+		{/if}
+	{/each}
 </div>
