c690a231191ebf3f863a21a7caa5fe0ff2498fc4
Edwin De La Cruz <edwinspire@gmail.com>
2023-08-23 00:15:36 -0500

    Correccion de bug en SQL 2

---
+++ build/server/chunks/1-b89ae09b.js
@@ -0,0 +1,9 @@
+const index = 1;
+let component_cache;
+const component = async () => component_cache ??= (await import('./error.svelte-7d779496.js')).default;
+const imports = ["_app/immutable/nodes/1.22b9f759.js","_app/immutable/chunks/scheduler.009eed4a.js","_app/immutable/chunks/index.cf223af7.js","_app/immutable/chunks/singletons.14a4d0b0.js","_app/immutable/chunks/index.2058b505.js"];
+const stylesheets = [];
+const fonts = [];
+
+export { component, fonts, imports, index, stylesheets };
+//# sourceMappingURL=1-b89ae09b.js.map
--- src/lib/api/gui/app/handler/sql.svelte
+++ src/lib/api/gui/app/handler/sql.svelte
@@ -1,105 +1,105 @@
 <script>
-  // @ts-nocheck
-  import { onMount } from "svelte";
-  import { Tab } from "@edwinspire/svelte-components";
-  import Vars from "../vars.svelte";
-  import EditorCode from "./editorCode.svelte";
-  import EditorCode2 from "./editorCode.svelte";
+	// @ts-nocheck
+	import { onMount } from 'svelte';
+	import { Tab } from '@edwinspire/svelte-components';
+	import Vars from '../vars.svelte';
+	import EditorCode from './editorCode.svelte';
+	import EditorCode2 from './editorCode.svelte';
 
-  /**
-   * @type {any}
-   */
-  export let code;
-  /**
-   * @type {EditorCode}
-   */
-  let fnEditorCode;
+	/**
+	 * @type {any}
+	 */
+	export let code;
+	/**
+	 * @type {EditorCode}
+	 */
+	let fnEditorCode;
 
-  let tabList = [
-    { label: "Query", isActive: true },
-    { label: "Connection Parameters" },
-    { label: "App Variables" },
-  ];
+	let tabList = [
+		{ label: 'Query', isActive: true },
+		{ label: 'Connection Parameters' },
+		{ label: 'App Variables' }
+	];
 
-  /**
-   * @type {EditorCode}
-   */
-  let fnEditorCode2;
-  let params_code = "{}";
-  let query_code = "SELECT 1+1;";
-  $: code, ParseCode();
+	/**
+	 * @type {EditorCode}
+	 */
+	let fnEditorCode2;
+	let params_code = '{}';
+	let query_code = 'SELECT 1+1;';
+	$: code, ParseCode();
 
-  function ParseCode() {
-    try {
-      let params = JSON.parse(code||{});
+	function ParseCode() {
+		try {
+			let params = JSON.parse(code || '{}');
 
-      if (params && params.query) {
-        query_code = params.query;
-      }
+			if (params && params.query) {
+				query_code = params.query;
+			}
 
-      if (params && params.config) {
-        params_code = JSON.stringify(params.config);
-      }
-    } catch (error) {
-      params_code = "{}";
-      query_code = "SELECT 1;";
-      console.error("Error", error);
-    }
-  }
+			if (params && params.config) {
+				params_code = JSON.stringify(params.config);
+			}
+		} catch (error) {
+			params_code = '{}';
+			query_code = 'SELECT 1;';
+			console.error('Error', error);
+		}
+	}
 
-  export function getCode() {
-    //fnEditorCode.apply();
-    let p = fnEditorCode.getCode();
-    let q = fnEditorCode2.getCode();
-    try {
-      let c = JSON.parse(p);
-      c.query = q;
-      return JSON.stringify(c, null, 2);
-    } catch (error) {
-      console.error("No se pudo parsear", error);
-      return code;
-    }
-  }
+	export function getCode() {
+		//fnEditorCode.apply();
+		let p = fnEditorCode.getCode();
+		let q = fnEditorCode2.getCode();
+		try {
+			let c = JSON.parse(p);
+			c.query = q;
+			return JSON.stringify(c, null, 2);
+		} catch (error) {
+			console.error('No se pudo parsear', error);
+			return code;
+		}
+	}
 
-  onMount(() => {
-    console.log(code);
-    ParseCode();
-  });
+	onMount(() => {
+		console.log(code);
+		ParseCode();
+	});
 </script>
 
 <Tab bind:tabs={tabList}>
-  <div class={tabList[0].isActive ? "" : "is-hidden"}>
-    <EditorCode2 lang="sql" bind:code={query_code} bind:this={fnEditorCode2}
-      ><div slot="message">
-        <div class="content is-small">
-          Query to be executed. The parameters must have a name like <span
-            style="font-style: oblique">$nameparameter</span
-          >
-          which bind to the values ​​you send in the request.
-          <div>
-            For more information you can consult the <a
-              href="https://sequelize.org/docs/v6/core-concepts/raw-queries/#bind-parameter"
-              >sequelize</a
-            > documentation.
-          </div>
-        </div>
-      </div></EditorCode2
-    >
-  </div>
+	<div class={tabList[0].isActive ? '' : 'is-hidden'}>
+		<EditorCode2 lang="sql" bind:code={query_code} bind:this={fnEditorCode2}
+			><div slot="message">
+				<div class="content is-small">
+					Query to be executed. The parameters must have a name like <span
+						style="font-style: oblique">$nameparameter</span
+					>
+					which bind to the values ​​you send in the request.
+					<div>
+						For more information you can consult the <a
+							href="https://sequelize.org/docs/v6/core-concepts/raw-queries/#bind-parameter"
+							>sequelize</a
+						> documentation.
+					</div>
+				</div>
+			</div></EditorCode2
+		>
+	</div>
 
-  <div class={tabList[1].isActive ? "" : "is-hidden"}>
-    <EditorCode lang="json" bind:code={params_code} bind:this={fnEditorCode}>
-      <div slot="message">
-        <div class="content is-small">
-          Configuration parameters used by sequelize, visit <a
-            href="https://sequelize.org/">https://sequelize.org/</a
-          > for more information.
-        </div>
-      </div>
-    </EditorCode>
-  </div>
+	<div class={tabList[1].isActive ? '' : 'is-hidden'}>
+		<EditorCode lang="json" bind:code={params_code} bind:this={fnEditorCode}>
+			<div slot="message">
+				<div class="content is-small">
+					Configuration parameters used by sequelize, visit <a href="https://sequelize.org/"
+						>https://sequelize.org/</a
+					> for more information.
+				</div>
+			</div>
+		</EditorCode>
+	</div>
 
-  <div class={tabList[2].isActive ? "" : "is-hidden"}>
-    <Vars />
-  </div>
+	<div class={tabList[2].isActive ? '' : 'is-hidden'}>
+		<Vars />
+	</div>
 </Tab>
