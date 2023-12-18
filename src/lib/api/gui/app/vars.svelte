a511d581a6e6fe31e415d686001a9229fa8b143d
Edwin De La Cruz <edwinspire@gmail.com>
2023-12-17 23:22:30 -0500

    se agrega boton para crear nueva variable de aplicacción

--- src/lib/api/gui/app/vars.svelte
+++ src/lib/api/gui/app/vars.svelte
@@ -10,6 +10,8 @@
 	export let environment = '';
 	let Datavars = {};
 	let fnEditorCodes = {};
+	let showNewVar = false;
+	let newVar = '';
 
 	listAppVars.subscribe((value) => {
 		console.log('listAppVars ->>>>', value);
@@ -44,10 +46,65 @@
 </script>
 
 <details>
-	<summary><strong>App Vars</strong></summary>
+	<summary><strong>App Vars - {environment}</strong></summary>
 
 	<div>
 		{#if Datavars && Datavars[environment]}
+			<div class="field has-addons">
+				<p class="control">
+					<!-- svelte-ignore a11y-missing-attribute -->
+					<a class="button is-static is-small"> {environment} variables </a>
+				</p>
+				<p class="control">
+					<button
+						class="button is-small"
+						on:click={() => {
+							showNewVar = true;
+							newVar = '';
+						}}
+					>
+						New
+					</button>
+				</p>
+
+				{#if showNewVar}
+					<p class="control">
+						<input
+							class="input is-small"
+							type="text"
+							placeholder="Variable name"
+							bind:value={newVar}
+						/>
+					</p>
+					<p class="control">
+						<button
+							class="button is-small is-success"
+							on:click={() => {
+								if (newVar && newVar.startsWith('$_VAR')) {
+									Datavars[environment][newVar] = {};
+									showNewVar = false;
+								} else {
+									alert('The name must start with $_VAR');
+								}
+							}}
+						>
+							Accept
+						</button>
+					</p>
+					<p class="control">
+						<button
+							class="button is-small is-danger"
+							on:click={() => {
+								showNewVar = false;
+								newVar = '';
+							}}
+						>
+							Cancel
+						</button>
+					</p>
+				{/if}
+			</div>
+
 			{#each Object.keys(Datavars[environment]) as varKey}
 				{#if editable}
 					<details>
@@ -56,7 +113,23 @@
 							bind:this={fnEditorCodes[varKey]}
 							lang={'json'}
 							code={JSON.stringify(Datavars[environment][varKey])}
-						/>
+							><span slot="message">
+								<button
+									class="button is-danger is-small"
+									on:click={() => {
+										// TODO: Hacer que funcione
+										if (confirm('Are you sure you want to permanently delete the variables?')) {
+											delete Datavars[environment][varKey];
+										}
+									}}
+								>
+									<span>Delete</span>
+									<span class="icon is-small">
+										<i class="fas fa-times"></i>
+									</span>
+								</button></span
+							></EditorCode
+						>
 					</details>
 				{:else}
 					<details>
