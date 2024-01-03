a3ae6b6ae2b2dc562e4a1fc48b55452f7a1ca16f
Edwin De La Cruz <edwinspire@gmail.com>
2024-01-02 22:41:16 -0500

    se agrega log

--- src/lib/api/db/sequelize.js
+++ src/lib/api/db/sequelize.js
@@ -2,18 +2,17 @@ import dotenv from 'dotenv';
 dotenv.config()
 import { Sequelize } from 'sequelize'
 
+const db_conn = process.env.DATABASE_URL_APIREST || "sqlite://apirest.sqlite3/";
+
 // @ts-ignore
-const dbsequelize = new Sequelize(process.env.DATABASE_URL_APIREST || "sqlite://apirest.sqlite3/", {
+const dbsequelize = new Sequelize(db_conn, {
   pool: {
     max: 10,
     min: 0,
     acquire: 30000,
     idle: 10000,
   },
-}) // Example for postgres
-
-
-
+}) 
 
 export default dbsequelize;
 
@@ -23,7 +22,7 @@ export default dbsequelize;
     await dbsequelize.authenticate()
     console.log('Connection has been established successfully.')
   } catch (error) {
-    console.error('Unable to connect to the database:', error)
+    console.error('Unable to connect to the database: '+db_conn, error)
   }
 
 })();
