a2f4404112b9dbc5cce4f7ae37c0be632cdcb453
Edwin De La Cruz <edwinspire@gmail.com>
2024-01-04 21:47:38 -0500

    Reconstruido

--- build/server/chunks/3-yQ-BeFnm.js
+++ build/server/chunks/3-yQ-BeFnm.js
@@ -1,9 +1,9 @@
 const index = 3;
 let component_cache;
 const component = async () => component_cache ??= (await import('./_page.svelte-BnqKTtxt.js')).default;
-const imports = ["_app/immutable/nodes/3.-7poVP42.js","_app/immutable/chunks/scheduler.aTojFtUW.js","_app/immutable/chunks/index.VwvfgrWf.js","_app/immutable/chunks/index.JCt9Du3o.js","_app/immutable/chunks/index.UaLPdvMp.js"];
+const imports = ["_app/immutable/nodes/3.sQYqPS3t.js","_app/immutable/chunks/scheduler.aTojFtUW.js","_app/immutable/chunks/index.VwvfgrWf.js","_app/immutable/chunks/index.JCt9Du3o.js","_app/immutable/chunks/index.UaLPdvMp.js"];
 const stylesheets = ["_app/immutable/assets/3.6A9vq023.css","_app/immutable/assets/all.Y-W6EoD-.css","_app/immutable/assets/index.FVGRIrGp.css"];
 const fonts = ["_app/immutable/assets/fa-brands-400.vfWL-IA9.woff2","_app/immutable/assets/fa-brands-400.7WSgTNmj.ttf","_app/immutable/assets/fa-regular-400.DjrD39cY.woff2","_app/immutable/assets/fa-regular-400.TBaJECdl.ttf","_app/immutable/assets/fa-solid-900.wCNuHzbQ.woff2","_app/immutable/assets/fa-solid-900.zNLXiXYJ.ttf","_app/immutable/assets/fa-v4compatibility.Gkfbzimj.woff2","_app/immutable/assets/fa-v4compatibility.BdHZVUy0.ttf"];
 
 export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=3-2FTguvEg.js.map
+//# sourceMappingURL=3-yQ-BeFnm.js.map
--- build/server/manifest.js
+++ build/server/manifest.js
@@ -10,11 +10,11 @@ return {
 	assets: new Set(["css/bulma/bulma-rtl.css","css/bulma/bulma-rtl.css.map","css/bulma/bulma-rtl.min.css","css/bulma/bulma.css","css/bulma/bulma.css.map","css/bulma/bulma.min.css","css/fontawesome/css/all.css","css/fontawesome/css/all.min.css","css/fontawesome/css/brands.css","css/fontawesome/css/brands.min.css","css/fontawesome/css/fontawesome.css","css/fontawesome/css/fontawesome.min.css","css/fontawesome/css/regular.css","css/fontawesome/css/regular.min.css","css/fontawesome/css/solid.css","css/fontawesome/css/solid.min.css","css/fontawesome/css/svg-with-js.css","css/fontawesome/css/svg-with-js.min.css","css/fontawesome/css/v4-font-face.css","css/fontawesome/css/v4-font-face.min.css","css/fontawesome/css/v4-shims.css","css/fontawesome/css/v4-shims.min.css","css/fontawesome/css/v5-font-face.css","css/fontawesome/css/v5-font-face.min.css","css/fontawesome/webfonts/fa-brands-400.ttf","css/fontawesome/webfonts/fa-brands-400.woff2","css/fontawesome/webfonts/fa-regular-400.ttf","css/fontawesome/webfonts/fa-regular-400.woff2","css/fontawesome/webfonts/fa-solid-900.ttf","css/fontawesome/webfonts/fa-solid-900.woff2","css/fontawesome/webfonts/fa-v4compatibility.ttf","css/fontawesome/webfonts/fa-v4compatibility.woff2","favicon.png","robots.txt","svelte-welcome.png","svelte-welcome.webp"]),
 	mimeTypes: {".css":"text/css",".map":"application/json",".ttf":"font/ttf",".woff2":"font/woff2",".png":"image/png",".txt":"text/plain",".webp":"image/webp"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.OHyziKg-.js","app":"_app/immutable/entry/app.XVngS1z6.js","imports":["_app/immutable/entry/start.OHyziKg-.js","_app/immutable/chunks/scheduler.aTojFtUW.js","_app/immutable/chunks/singletons._JWM_so5.js","_app/immutable/chunks/index.UaLPdvMp.js","_app/immutable/entry/app.XVngS1z6.js","_app/immutable/chunks/scheduler.aTojFtUW.js","_app/immutable/chunks/index.VwvfgrWf.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.TBvEyTLG.js","app":"_app/immutable/entry/app.frLULNve.js","imports":["_app/immutable/entry/start.TBvEyTLG.js","_app/immutable/chunks/scheduler.aTojFtUW.js","_app/immutable/chunks/singletons.l2DTmHXv.js","_app/immutable/chunks/index.UaLPdvMp.js","_app/immutable/entry/app.frLULNve.js","_app/immutable/chunks/scheduler.aTojFtUW.js","_app/immutable/chunks/index.VwvfgrWf.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('./chunks/0-lwkYCGru.js')),
-			__memo(() => import('./chunks/1-yKJldtrW.js')),
-			__memo(() => import('./chunks/3-2FTguvEg.js'))
+			__memo(() => import('./chunks/1-hJubE1y9.js')),
+			__memo(() => import('./chunks/3-yQ-BeFnm.js'))
 		],
 		routes: [
 			{
