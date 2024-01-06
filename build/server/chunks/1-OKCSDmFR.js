8ea0615978fc18c3d1e27b427dc7ad8f732a84f3
Edwin De La Cruz <edwinspire@gmail.com>
2024-01-05 19:08:54 -0500

    Se mueve svelte-components como dependencia del proyecto

---
+++ build/server/chunks/1-OKCSDmFR.js
@@ -0,0 +1,9 @@
+const index = 1;
+let component_cache;
+const component = async () => component_cache ??= (await import('./error.svelte-kn34lKjN.js')).default;
+const imports = ["_app/immutable/nodes/1.y6yK-3hf.js","_app/immutable/chunks/scheduler.aTojFtUW.js","_app/immutable/chunks/index.VwvfgrWf.js","_app/immutable/chunks/singletons.OfK1hqCM.js","_app/immutable/chunks/index.UaLPdvMp.js"];
+const stylesheets = [];
+const fonts = [];
+
+export { component, fonts, imports, index, stylesheets };
+//# sourceMappingURL=1-OKCSDmFR.js.map
--- build/server/manifest.js
+++ build/server/manifest.js
@@ -7,13 +7,13 @@ function __memo(fn) {
 return {
 	appDir: "_app",
 	appPath: "_app",
-	assets: new Set(["css/_bulma/bulma-rtl.css","css/_bulma/bulma-rtl.css.map","css/_bulma/bulma-rtl.min.css","css/_bulma/bulma.css","css/_bulma/bulma.css.map","css/_bulma/bulma.min.css","css/_fontawesome/css/all.css","css/_fontawesome/css/all.min.css","css/_fontawesome/css/brands.css","css/_fontawesome/css/brands.min.css","css/_fontawesome/css/fontawesome.css","css/_fontawesome/css/fontawesome.min.css","css/_fontawesome/css/regular.css","css/_fontawesome/css/regular.min.css","css/_fontawesome/css/solid.css","css/_fontawesome/css/solid.min.css","css/_fontawesome/css/svg-with-js.css","css/_fontawesome/css/svg-with-js.min.css","css/_fontawesome/css/v4-font-face.css","css/_fontawesome/css/v4-font-face.min.css","css/_fontawesome/css/v4-shims.css","css/_fontawesome/css/v4-shims.min.css","css/_fontawesome/css/v5-font-face.css","css/_fontawesome/css/v5-font-face.min.css","css/_fontawesome/webfonts/fa-brands-400.ttf","css/_fontawesome/webfonts/fa-brands-400.woff2","css/_fontawesome/webfonts/fa-regular-400.ttf","css/_fontawesome/webfonts/fa-regular-400.woff2","css/_fontawesome/webfonts/fa-solid-900.ttf","css/_fontawesome/webfonts/fa-solid-900.woff2","css/_fontawesome/webfonts/fa-v4compatibility.ttf","css/_fontawesome/webfonts/fa-v4compatibility.woff2","favicon.png","robots.txt","svelte-welcome.png","svelte-welcome.webp"]),
-	mimeTypes: {".css":"text/css",".map":"application/json",".ttf":"font/ttf",".woff2":"font/woff2",".png":"image/png",".txt":"text/plain",".webp":"image/webp"},
+	assets: new Set(["favicon.png","robots.txt","svelte-welcome.png","svelte-welcome.webp"]),
+	mimeTypes: {".png":"image/png",".txt":"text/plain",".webp":"image/webp"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.8YbVnx2w.js","app":"_app/immutable/entry/app.dO7oVOTr.js","imports":["_app/immutable/entry/start.8YbVnx2w.js","_app/immutable/chunks/scheduler.aTojFtUW.js","_app/immutable/chunks/singletons.D2XZhUs-.js","_app/immutable/chunks/index.UaLPdvMp.js","_app/immutable/entry/app.dO7oVOTr.js","_app/immutable/chunks/scheduler.aTojFtUW.js","_app/immutable/chunks/index.VwvfgrWf.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.hDuFhqOM.js","app":"_app/immutable/entry/app.rWr5P2-x.js","imports":["_app/immutable/entry/start.hDuFhqOM.js","_app/immutable/chunks/scheduler.aTojFtUW.js","_app/immutable/chunks/singletons.OfK1hqCM.js","_app/immutable/chunks/index.UaLPdvMp.js","_app/immutable/entry/app.rWr5P2-x.js","_app/immutable/chunks/scheduler.aTojFtUW.js","_app/immutable/chunks/index.VwvfgrWf.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('./chunks/0-NXxPxFqI.js')),
-			__memo(() => import('./chunks/1-T9HgasnP.js')),
+			__memo(() => import('./chunks/1-OKCSDmFR.js')),
 			__memo(() => import('./chunks/3-ORPpgPYp.js'))
 		],
 		routes: [
